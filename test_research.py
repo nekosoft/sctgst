5c21c3db766d848b63cff2d0dea01a7c1a0b897e
Yianna Paris <24336984+nekosoft@users.noreply.github.com>
2023-12-19 19:34:26 +0100

    Create test_research.py
    Example unit test

---
+++ test_research.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import research
+
+def test_welcome():
+  assert welcome() == "Welcome!", "Should print "\Welcome!"\"
+
+if __name__ = "__main__":
+  test_welcome()
+  print("Everything passed")
+  
